b17edd4921ca337cfa82992215b97f2defad6820
Stephanie Smith <stephsmithcode@gmail.com>
2019-02-24 09:27:56 -0800

    passing more props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import faker from 'faker';
 
-const CommentDetail = ({ author, comment }) => {
+const CommentDetail = ({ author, img, comment, timeAgo }) => {
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()}/>
+                <img alt="avatar" src={img}/>
             </a>
             <div className="content">
                 <a href="/" className="author">
                     {author}
                 </a>
                 <div className="metadata">
-                    <span className="date">Today at 6:00PM</span>
+                    <span className="date">{timeAgo}</span>
                 </div>
                 <div className="text">{comment}</div>
             </div>
--- src/index.js
+++ src/index.js
@@ -6,9 +6,21 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author={faker.name.firstName()} comment={faker.lorem.sentence()}/>
-            <CommentDetail author={faker.name.firstName()} comment={faker.lorem.sentence()}/>
-            <CommentDetail author={faker.name.firstName()} comment={faker.lorem.sentence()}/>
+            <CommentDetail
+                author={faker.name.firstName()}
+                img={faker.image.avatar()}
+                comment={faker.lorem.sentence()}
+                timeAgo="Today at 4:34PM"/>
+            <CommentDetail
+                author={faker.name.firstName()}
+                img={faker.image.avatar()}
+                comment={faker.lorem.sentence()}
+                timeAgo="Today at 10:23AM"/>
+            <CommentDetail
+                author={faker.name.firstName()}
+                img={faker.image.avatar()}
+                comment={faker.lorem.sentence()}
+                timeAgo="Yesterday at 6:03PM"/>
         </div>
     )
 }
