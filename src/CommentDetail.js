ac263d1eb9f5411fc71fa961b18abb05ed4d5623
Stephanie Smith <stephsmithcode@gmail.com>
2019-02-24 09:29:32 -0800

    fixing tabs

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
 const CommentDetail = ({ author, img, comment, timeAgo }) => {
-    return (
-        <div className="comment">
-            <a href="/" className="avatar">
-                <img alt="avatar" src={img}/>
-            </a>
-            <div className="content">
-                <a href="/" className="author">
-                    {author}
-                </a>
-                <div className="metadata">
-                    <span className="date">{timeAgo}</span>
-                </div>
-                <div className="text">{comment}</div>
-            </div>
-        </div>
-    )
+	return (
+		<div className="comment">
+			<a href="/" className="avatar">
+				<img alt="avatar" src={img}/>
+			</a>
+			<div className="content">
+				<a href="/" className="author">
+					{author}
+				</a>
+				<div className="metadata">
+					<span className="date">{timeAgo}</span>
+				</div>
+				<div className="text">{comment}</div>
+			</div>
+		</div>
+	)
 }
 
 export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -4,25 +4,25 @@ import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <CommentDetail
-                author={faker.name.firstName()}
-                img={faker.image.avatar()}
-                comment={faker.lorem.sentence()}
-                timeAgo="Today at 4:34PM"/>
-            <CommentDetail
-                author={faker.name.firstName()}
-                img={faker.image.avatar()}
-                comment={faker.lorem.sentence()}
-                timeAgo="Today at 10:23AM"/>
-            <CommentDetail
-                author={faker.name.firstName()}
-                img={faker.image.avatar()}
-                comment={faker.lorem.sentence()}
-                timeAgo="Yesterday at 6:03PM"/>
-        </div>
-    )
+	return (
+		<div className="ui container comments">
+			<CommentDetail
+				author={faker.name.firstName()}
+				img={faker.image.avatar()}
+				comment={faker.lorem.sentence()}
+				timeAgo="Today at 4:34PM"/>
+			<CommentDetail
+				author={faker.name.firstName()}
+				img={faker.image.avatar()}
+				comment={faker.lorem.sentence()}
+				timeAgo="Today at 10:23AM"/>
+			<CommentDetail
+				author={faker.name.firstName()}
+				img={faker.image.avatar()}
+				comment={faker.lorem.sentence()}
+				timeAgo="Yesterday at 6:03PM"/>
+		</div>
+	)
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
